79b0df0eec5f3be7bb401ccaf50cbd2f2624373d
claraamaroo <165063276+claraamaroo@users.noreply.github.com>
2024-10-18 14:57:34 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao( 
+     'Curiosidades sobre a Disney',
+"Em que ano foi criado a Disney?",
+"Em 16/10/1923"
+)
+criaCartao(
+"Banda",
+"Em qual banda do rock que o cantor Freddie Mercury foi vocalista?",
+
+
+"Queen!"
+)
+
+criaCartao(
+"Portugues",
+"Qual a letra que antecede a letra O no alfabeto",
+
+'letra N',
+)
+
+
+criaCartao(
+    "Esporte",
+    'Onde sao disputadas as lutas de judo?',
+    "No tatame",
+)
+
+
+
+
+
+)
+
+
+
+
+
+
+
+
+)
+)
+
+
+
